d88d7aac3b3788048428932ad42c0e038ef30aaf
TomD53 <tomtomgaming101@gmail.com>
2021-09-09 22:04:30 +0100

    use raw data, add packet loss

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,20 @@
 
 // this sketch is adapted from the following example https://github.com/jrowberg/i2cdevlib/blob/master/Arduino/MPU6050/examples/MPU6050_DMP6_using_DMP_V6.12/MPU6050_DMP6_using_DMP_V6.12.ino
 
-struct Payload
+struct Payload // MAX 32 BYTES
 {
-  float yaw;
-  float pitch;
-  float roll;
+  // 12 bytes of MPU data
+  int16_t ax, ay, az; // acceleration values in all 3 axis (-32768 to 32767)
+  int16_t gx, gy, gz; // gyroscope acceleration values in all 3 axis (-32768 to 32767)
 
-  short aquart_x;
-  short aquart_y;
-  short aquart_z;
+  float pressure; // 4 bytes
+  int altitude;   // 2 bytes
+  float temp;     // 4 bytes
 
-  long pressure;
-  int altitude;
-  float temp;
+  unsigned int vcc;       // 2 bytes
+  unsigned int packet_id; // 2 bytes
 
-  unsigned int vcc;
-  unsigned int packet_id;
+  // we have 26 bytes total
 };
 
 Payload payload;
@@ -55,52 +53,30 @@ MPU6050 mpu;
 BME280 temp_sensor;
 ; // BMP280 Pressure sensor module library
 
-#define INTERRUPT_PIN 2 // use pin 2 on Arduino Uno & most boards
-#define LED_PIN 13      // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 bool blinkState = false;
-
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;        // [w, x, y, z]         quaternion container
-VectorInt16 aa;      // [x, y, z]            accel sensor measurements
-VectorInt16 gy;      // [x, y, z]            gyro sensor measurements
-VectorInt16 aaReal;  // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld; // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity; // [x, y, z]            gravity vector
-float euler[3];      // [psi, theta, phi]    Euler angle container
-float ypr[3];        // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
-void dmpDataReady()
-{
-  mpuInterrupt = true;
-}
-
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
 
 void setup()
 {
+  // initialize serial communication
+  Serial.begin(9600);
+
+  Serial.print("Payload size: ");
+  Serial.println(int(sizeof(payload)));
+
+  Serial.println("Setting up radio");
 
   radio.begin();
-  radio.setDataRate(RF24_1MBPS);
+  Serial.println("Setting radio data rate");
+  radio.setDataRate(RF24_250KBPS);
+  Serial.println("Setting radio power");
   radio.setPALevel(RF24_PA_MAX);
+  Serial.println("Opening writing pipe");
   radio.openWritingPipe(pipe);
-  radio.printDetails();
 
-  // join I2C bus (I2Cdev library doesn't do this automatically)
+  Serial.println("Joining i2c bus");
+
+// join I2C bus (I2Cdev library doesn't do this automatically)
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
   Wire.begin();
   Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
@@ -108,71 +84,31 @@ void setup()
   Fastwire::setup(400, true);
 #endif
 
-  // initialize serial communication
-  Serial.begin(9600);
-  while (!Serial)
-    ; // wait for Leonardo enumeration, others continue immediately
+  Serial.println("Finished joining i2c bus");
 
   // initialize device
   Serial.println(F("Initializing I2C devices..."));
   mpu.initialize();
-  mpu.setSleepEnabled(false);
-  pinMode(INTERRUPT_PIN, INPUT);
+
+  /*
+  0 = +/- 2g
+  1 = +/- 4g
+  2 = +/- 8g
+  3 = +/- 16g
+  */
+  mpu.setFullScaleAccelRange(3);
+
+  /*
+  0 = +/- 250 degrees/sec
+  1 = +/- 500 degrees/sec
+  2 = +/- 1000 degrees/sec
+  3 = +/- 2000 degrees/sec
+  */
+  mpu.setFullScaleGyroRange(3);
 
   // verify connection
   Serial.println(F("Testing device connections..."));
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-  // load and configure the DMP
-  Serial.println(F("Initializing DMP..."));
-  devStatus = mpu.dmpInitialize();
-
-  // supply your own gyro offsets here, scaled for min sensitivity
-  /*
-  mpu.setXGyroOffset(106);
-  mpu.setYGyroOffset(-36);
-  mpu.setZGyroOffset(-1);
-  mpu.setXAccelOffset(-2349);
-  mpu.setYAccelOffset(475);
-  mpu.setZAccelOffset(2707);
-  */
-
-  // make sure it worked (returns 0 if so)
-  if (devStatus == 0)
-  {
-    // Calibration Time: generate offsets and calibrate our MPU6050
-    mpu.CalibrateAccel(6);
-    mpu.CalibrateGyro(6);
-    Serial.println();
-    mpu.PrintActiveOffsets();
-    // turn on the DMP, now that it's ready
-    Serial.println(F("Enabling DMP..."));
-    mpu.setDMPEnabled(true);
-
-    // enable Arduino interrupt detection
-    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-    Serial.println(F(")..."));
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-    mpuIntStatus = mpu.getIntStatus();
-
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    Serial.println(F("DMP ready! Waiting for first interrupt..."));
-    dmpReady = true;
-
-    // get expected DMP packet size for later comparison
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  }
-  else
-  {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
-    Serial.print(F("DMP Initialization failed (code "));
-    Serial.print(devStatus);
-    Serial.println(F(")"));
-  }
 
   temp_sensor.setI2CAddress(0x76);
   temp_sensor.beginI2C();
@@ -183,31 +119,33 @@ void setup()
   pinMode(LED_PIN, OUTPUT);
 }
 
-unsigned int readVcc() {
-  // https://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
-  // Read 1.1V reference against AVcc
-  // set the reference to Vcc and the measurement to the internal 1.1V reference
-  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
-    ADMUX = _BV(MUX5) | _BV(MUX0);
-  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
-    ADMUX = _BV(MUX3) | _BV(MUX2);
-  #else
-    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  #endif  
+unsigned int readVcc()
+{
+// https://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
+// Read 1.1V reference against AVcc
+// set the reference to Vcc and the measurement to the internal 1.1V reference
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#elif defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  ADMUX = _BV(MUX5) | _BV(MUX0);
+#elif defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+#else
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#endif
 
-  delay(2); // Wait for Vref to settle
+  delay(2);            // Wait for Vref to settle
   ADCSRA |= _BV(ADSC); // Start conversion
-  while (bit_is_set(ADCSRA,ADSC)); // measuring
+  while (bit_is_set(ADCSRA, ADSC))
+    ; // measuring
 
-  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t low = ADCL;  // must read ADCL first - it then locks ADCH
   uint8_t high = ADCH; // unlocks both
 
-  unsigned int result = (high<<8) | low;
+  unsigned int result = (high << 8) | low;
 
   result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
-  return result; // Vcc in millivolts
+  return result;              // Vcc in millivolts
 }
 
 long last_start_time = micros();
@@ -218,88 +156,39 @@ unsigned int packet_id;
 
 void loop()
 {
-  // if programming failed, don't try to do anything
-  if (!dmpReady)
-    return;
-  // read a packet from FIFO
-  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer))
-  { // Get the Latest packet
+  mpu.getMotion6(
+      &payload.ax, &payload.ay, &payload.az,
+      &payload.gx, &payload.gy, &payload.gz
+  );
+  
+  payload.pressure = temp_sensor.readFloatPressure();
 
-    // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    payload.yaw = ypr[0] * 180 / M_PI;
-    payload.pitch = ypr[1] * 180 / M_PI;
-    payload.roll = ypr[2] * 180 / M_PI;
+  payload.altitude = temp_sensor.readFloatAltitudeMeters();
 
-    // display initial world-frame acceleration, adjusted to remove gravity
-    // and rotated based on known orientation from quaternion
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetAccel(&aa, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-    payload.aquart_x = aaWorld.x;
-    payload.aquart_y = aaWorld.y;
-    payload.aquart_z = aaWorld.z;
+  payload.temp = temp_sensor.readTempC();
 
-    payload.pressure = temp_sensor.readFloatPressure();
+  ++packet_id;
+  payload.packet_id = packet_id;
 
-    payload.altitude = temp_sensor.readFloatAltitudeMeters();
+  radio.write(&payload, sizeof(Payload));
 
-    payload.temp = temp_sensor.readTempC();
+  ++packets_sent;
 
-    ++packet_id;
-    payload.packet_id = packet_id;
+  timedelta = micros() - last_start_time;
+  if (timedelta > 1000000)
+  {
+    packets_per_second = packets_sent / (timedelta / 1000000.000);
+    Serial.print("PPS: ");
+    Serial.println(packets_per_second);
+    last_start_time = micros();
+    packets_sent = 0;
 
-    // print the values to the console
-    /*
-    Serial.print(payload.yaw);
-    Serial.print(",");
-    Serial.print(payload.pitch);
-    Serial.print(",");
-    Serial.print(payload.roll);
-    Serial.print(",");
-    Serial.print(payload.aquart_x);
-    Serial.print(",");
-    Serial.print(payload.aquart_y);
-    Serial.print(",");
-    Serial.print(payload.aquart_z);
-    Serial.print(",");
-    Serial.print(payload.pressure);
-    Serial.print(",");
-    Serial.print(payload.altitude);
-    Serial.print(",");
-    Serial.print(payload.temp);
+    payload.vcc = readVcc();
+    //Serial.print("Packet ID: ");
+    //Serial.println(payload.packet_id);
 
-    Serial.println();
-    */
-
-    radio.write( &payload, sizeof(Payload) );
-
-    ++packets_sent;
-
-    timedelta = micros() - last_start_time;
-    if (timedelta > 1000000)
-    {
-      packets_per_second = packets_sent / (timedelta / 1000000.000);
-      Serial.print("Iterations: ");
-      Serial.println(packets_per_second);
-      last_start_time = micros();
-      packets_sent = 0;
-      
-      payload.vcc = readVcc();
-      Serial.print("Packet ID: ");
-      Serial.println(payload.packet_id);
-
-
-      Serial.print("Payload size: ");
-      Serial.println(sizeof(Payload));
-
-      // blink LED to indicate activity
-      blinkState = !blinkState;
-      digitalWrite(LED_PIN, blinkState);
-    }
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
   }
 }
